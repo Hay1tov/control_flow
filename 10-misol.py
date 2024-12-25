2ed935e688e7998d99293f7e5f1cb63674035a91
Hay1tov <eldorh35@gmail.com>
2024-12-25 18:00:22 +0500

    Bajarildi

--- 10-misol.py
+++ 10-misol.py
@@ -1,6 +1,10 @@
 text = input("Matnni kiriting: ")
 
 words = text.split()
+max_word = text[0]
 
-long_word = max(words, key=len)
-print("Eng uzun so'z:", long_word)
+for word in words:
+    if len(word) > len(max_word):
+        max_word = word
+
+print("Eng uzun so'z:", max_word)
